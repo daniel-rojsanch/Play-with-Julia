f393d299eb854d915e33fbf62015917dc932e0d1
Daniel Rojas Sánchez <daniel.rojsanch@gmail.com>
2023-03-04 06:46:47 -0600

    🐝

--- Parte1-Basico/Funciones.jl
+++ Parte1-Basico/Funciones.jl
@@ -1,2 +1,10 @@
 ## Vamos a poner a prueba todo lo que hemos visto hasta ahora
 ## creando una funcion para calcular la Winsorized-Mean
+
+#- Creamos una funcion abeja
+function 🐝(x)
+    println("Abeja es ", 2x)
+end
+
+#--- Evaluamos
+🐝(3)
