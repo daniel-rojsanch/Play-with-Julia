bb5090d945331b907ef907035f711d1c9346494b
Daniel Rojas Sánchez <daniel.rojsanch@gmail.com>
2023-03-25 14:54:57 -0600

    work 🐮

---
+++ Julia for Data Analysis/data frames/First steps with data frames.jl
@@ -0,0 +1,9 @@
+## you will learn the basic priciples of working with data frames
+## provided by DataFrames packages
+
+## Data Frames objects are flexible data structures that allow
+## you to work with tabular data
+
+## Each row has the same number of cells and provides information
+## about one observation.
+
