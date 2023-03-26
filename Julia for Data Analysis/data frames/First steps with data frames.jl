739cd6667a1999b55c82294a94279b83b6a0192f
Daniel Rojas Sánchez <daniel.rojsanch@gmail.com>
2023-03-25 18:06:30 -0600

    work 🐮

--- Julia for Data Analysis/data frames/First steps with data frames.jl
+++ Julia for Data Analysis/data frames/First steps with data frames.jl
@@ -7,3 +7,47 @@
 ## Each row has the same number of cells and provides information
 ## about one observation.
 
+
+
+## To work with database of puzzles, 
+## we first need to download it from the web
+## uncompress
+## and read it into a DataFrame
+
+
+import Downloads
+
+#@info Downloads.download("https://database.lichess.org/lichess_db_puzzle.csv.bz2",
+#"puzzles.csv.bz2")
+
+Downloads.download("https://raw.githubusercontent.com/daniel-rojsanch/Gallery-R/main/data/FIFA2.csv", "data/fifa.csv")
+
+## Una forma de comprobar si tenemos el archivo
+isfile("data/fifa.csv") 
+
+
+## otra manera y si no esta pues que nos lo descargue.
+if isfile("data/fifa.csv")
+    @info "ya exite el archivo"
+else
+    @info "vamos a descargarlo"
+    Downloads.download("https://raw.githubusercontent.com/daniel-rojsanch/Gallery-R/main/data/FIFA2.csv", "data/fifa.csv")
+end
+
+
+
+
+## Loading de data as DataFrame
+using CSV
+using DataFrames
+
+
+data = CSV.read("data/fifa.csv", DataFrame);
+data
+
+names(data)
+
+using Plots
+using StatsPlots
+@df data scatter(:Age, :Overall)
+
