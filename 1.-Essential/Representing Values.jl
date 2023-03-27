60d3b0f75e26b10fda5ed0ba4d8136f0a4ee6dcf
Daniel Rojas Sánchez <daniel.rojsanch@gmail.com>
2023-03-27 14:53:30 -0600

    essential julia 💀

---
+++ 1.-Essential/Representing Values.jl
@@ -0,0 +1,27 @@
+## we first need to learn how julia represents numbers and vector.
+## every value is a result of evaluation of some julia expression 
+## here are a few basic example values created by evaluation of literals
+
+
+## creating values by evaluating literals
+1
+true
+"Hello world!";
+1.0
+[1, 4, 5]
+
+
+## checking types of values 
+
+typeof(1)
+typeof(0.1)
+typeof("Hello world!")
+typeof([1, 4, 5])
+[1 4 5] == [1, 4, 5]
+typeof([1 4 5])
+
+
+
+
+
+
